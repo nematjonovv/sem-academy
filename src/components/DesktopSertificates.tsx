40bc0d51109665d73f1590369a6f26864d670f9e
Hikmatillo <hikmatillonematjonovv19@gmail.com>
2026-06-08 11:32:41 +0500

    add:certifiacate title

--- src/components/DesktopSertificates.tsx
+++ src/components/DesktopSertificates.tsx
@@ -1,24 +1,36 @@
+import Link from "next/link";
 import certificates from "../data/certificates";
 import CertCard from "./CertCard";
 
 function DesktopSertificates() {
   return (
-    <div
-      className="overflow-hidden py-10 mt-10 flex flex-col gap-15"
-      style={{
-        maskImage: "linear-gradient(to right, transparent 0%, black 8%, black 92%, transparent 100%)",
-        WebkitMaskImage: "linear-gradient(to right, transparent 0%, black 8%, black 92%, transparent 100%)",
-      }}
-    >
-      <div className="flex gap-5 w-max animate-marquee hover:[animation-play-state:paused]">
-        {[...certificates, ...certificates].map((cert, i) => (
-          <CertCard key={`${cert.id}-${i}`} cert={cert} />
-        ))}
+    <div className="w-full mt-50">
+      <div className="container flex justify-between items-center">
+        <h2 className="text-4xl font-jakarta font-extrabold  text-black mb-5">
+          O'quvchilarning <span className="text-(--primary)">TOPIK</span> natijalari
+        </h2>
+
+        <Link href="#" className="text-(--neutral) bg-(--primary) font-vietnam text-sm py-3 px-6 rounded-md hover:bg-(--primary)/90 transition-colors">
+          Baxtlilar orasida bo'lishi uchun siz ham yoziling
+        </Link>
       </div>
-      <div className="flex flex-row-reverse gap-5 w-max animate-marquee-reverse hover:[animation-play-state:paused]">
-        {[...certificates, ...certificates].map((cert, i) => (
-          <CertCard key={`${cert.id}-${i}`} cert={cert} />
-        ))}
+      <div
+        className="overflow-hidden py-10 mt-10 flex flex-col gap-15"
+        style={{
+          maskImage: "linear-gradient(to right, transparent 0%, black 8%, black 92%, transparent 100%)",
+          WebkitMaskImage: "linear-gradient(to right, transparent 0%, black 8%, black 92%, transparent 100%)",
+        }}
+      >
+        <div className="flex gap-5 w-max animate-marquee hover:[animation-play-state:paused]">
+          {[...certificates, ...certificates].map((cert, i) => (
+            <CertCard key={`${cert.id}-${i}`} cert={cert} />
+          ))}
+        </div>
+        <div className="flex flex-row-reverse gap-5 w-max animate-marquee-reverse hover:[animation-play-state:paused]">
+          {[...certificates, ...certificates].map((cert, i) => (
+            <CertCard key={`${cert.id}-${i}`} cert={cert} />
+          ))}
+        </div>
       </div>
     </div>
   );
